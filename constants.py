58494c6e4a65f2b66c4e1f7acf6f4b82fb6827fb
Arnav Mummineni <arnav.mummineni@gmail.com>
2021-09-22 18:18:40 -0400

    Made constants read-only

--- constants.py
+++ constants.py
@@ -1,17 +1,16 @@
 
+class Constant():
+    def __setattr__(self, name: str, value) -> None:
+        pass
 
-class Interface():
+class Interface(Constant):
     kDriverControllerPort = 0
     kManipControllerPort = 1
 
-class Drivetrain():
+class Drivetrain(Constant):
     kLeftMotorIDs = []
     kRightMotorIDs = []
 
-class Elevator():
+class Elevator(Constant):
     kMotorIDs = []
-    kPIDConstants = {
-        'Kp': 0,
-        'Ki': 0,
-        'Kd': 0,
-    }
+    kPIDConstants = {'Kp': 0, 'Ki': 0, 'Kd': 0}
