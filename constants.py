2ce48a406a9c3efea1775b38bebdf472dcf1ded6
RCoder01 <arnav.mummineni@gmail.com>
2021-09-24 00:03:43 -0400

    New _ConstantsClass

--- constants.py
+++ constants.py
@@ -1,13 +1,55 @@
 from collections import namedtuple
-from typing import _T, Any, Type
+from typing import _T, Any, Iterator, Type, Union, overload
 
 
-class _Constants_Set():
-    def __init__(values: dict = {}):
+class _ConstantClass(): ...
+
+
+class _Constant():
+    def __init__(self, values: dict = {}):
+        self._consts = {}
         for k, v in values.items():
+            
+            #Key must be a string
             if not isinstance(k, str):
                 raise TypeError("Constant keys must be of type 'str'")
             
+            if isinstance(v, dict):
+                self._consts[k] = _Constant(v)
+            elif isinstance(v, list):
+                self._consts[k] = tuple(v)
+            else:
+                self._consts[k] = v
+
+    @overload
+    def __getitem__(self, name: str) -> Any:
+        return self._consts[name]
+
+    @overload
+    def __getitem__(self, name: tuple[str]) -> Any:
+        if len(name) == 1:
+            return self[name[0]]
+        return self[name[0]][name[1:]]
+
+    def __dict__(self) -> dict:
+        _dict = {}
+        for k, v in self._consts:
+            if isinstance(v, _Constant):
+                v = dict(_Constant)
+            _dict[k] = v
+        return _dict
+
+    def __iter__(self) -> Iterator:
+        return self.__dict__().items()
+
+    def __str__(self) -> Any:
+        return str(self._consts)
+
+    def __getattribute__(self, name: str) -> Any:
+        self[name]
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        raise AttributeError('Constants cannot be changed')
 
 '''
 class _BaseConstant():
