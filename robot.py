64289fa664b9bae8ceda9a3a8284e903c705d060
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2021-12-14 10:37:10 -0500

    Remove main content

--- robot.py
+++ robot.py
@@ -1,7 +1,6 @@
 import commands2
 import wpilib
 import wpilib.drive
-import ctre
 
 class Robot(commands2.TimedCommandRobot):
     def robotInit(self):
--- src/frc/commands/__init__.py
+++
@@ -1,7 +0,0 @@
-from frc.commands import (
-    _drivetrain as drivetrain,
-)
-
-from frc.commands.commands import (
-    AutonomousCommand,
-)
