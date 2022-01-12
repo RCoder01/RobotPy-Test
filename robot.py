a22188b09241147a1e66600e0479a8860a531a2e
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2022-01-12 15:38:37 -0500

    Fix robot code

--- robot.py
+++ robot.py
@@ -1,22 +1,26 @@
+from rev import CANSparkMax, MotorType
 import commands2
 import wpilib
+import wpilib.interfaces
 import wpilib.drive
 
 class Robot(commands2.TimedCommandRobot):
     def robotInit(self):
-        print('init?')
-        # self.leftMotors = [ctre.WPI_TalonFX(ID) for ID in (15, 14, 13)]
-        # self.rightMotors = [ctre.WPI_TalonFX(ID) for ID in (1, 2, 20)]
-        # self.leftMotorController = wpilib.SpeedControllerGroup(*self.leftMotors)
-        # self.rightMotorController = wpilib.SpeedControllerGroup(*self.rightMotors)
-        # self.drivetrain = wpilib.drive.DifferentialDrive(self.leftMotorController, self.rightMotorController)
+        self.leftMotors = [CANSparkMax(ID, MotorType.kBrushless) for ID in (15, 14, 13)]
+        self.rightMotors = [CANSparkMax(ID, MotorType.kBrushless) for ID in (1, 2, 20)]
+        self.leftMotorController = wpilib.SpeedControllerGroup(*self.leftMotors)
+        self.rightMotorController = wpilib.SpeedControllerGroup(*self.rightMotors)
+        self.drivetrain = wpilib.drive.DifferentialDrive(self.leftMotorController, self.rightMotorController)
         
-        self.driver = wpilib.Joystick(0)
+        self.driver = wpilib.XboxController(0)
     
     def teleopPeriodic(self):
-        # self.drivetrain.arcadeDrive(self.driver.getY(), self.driver.getX()) # Y is forward/backward, X is left/right
-        wpilib.SmartDashboard.putNumber("Driver Y", self.driver.getY())
-        wpilib.SmartDashboard.putNumber("Driver X", self.driver.getX())
+        self.drivetrain.arcadeDrive(
+            self.driver.getY(wpilib.interfaces.GenericHID.Hand.kRightHand),
+            self.driver.getX(wpilib.interfaces.GenericHID.Hand.kLeftHand)
+        ) # Y is forward/backward, X is left/right
+        wpilib.SmartDashboard.putNumber("Driver Y", self.driver.getY(wpilib.interfaces.GenericHID.Hand.kRightHand))
+        wpilib.SmartDashboard.putNumber("Driver X", self.driver.getX(wpilib.interfaces.GenericHID.Hand.kLeftHand))
 
 if __name__ == '__main__':
     wpilib.run(Robot)
